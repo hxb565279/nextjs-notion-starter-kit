4e5524243160c5d9260799572e1b7422346ae874
LIGHT <63327266+hxb565279@users.noreply.github.com>
2023-01-21 17:17:35 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '47825ff2784842e3a4a3b15b41aca6ae',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  domain: 'https://hxb-22779799.notion.site/47825ff2784842e3a4a3b15b41aca6ae?v=122aefb49d814203b33425215f6bfb89',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
